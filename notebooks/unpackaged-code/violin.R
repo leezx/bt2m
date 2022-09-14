1976a8483e14095ff96667dd885e294c00ee42a5
Zstar <zxli@hku.hk>
2022-09-14 14:33:35 +0800

    Update violin.R

--- notebooks/unpackaged-code/violin.R
+++ notebooks/unpackaged-code/violin.R
@@ -5,35 +5,80 @@
 ## remove the x-axis text and tick
 ## plot.margin to adjust the white space between each plot.
 ## ... pass any arguments to VlnPlot in Seurat
-modify_vlnplot <- function(obj,
+StackedVlnPlot.rowCluster <- function(obj, features,
+                           pt.size = 0,
+                           plot.margin = unit(c(-0.75, 0, -0.75, 0), "cm"),
+                           ...) {
+  # addition funcitons
+  modify_vlnplot <- function(obj,
                            feature,
                            pt.size = 0,
                            plot.margin = unit(c(-0.75, 0, -0.75, 0), "cm"),
                            ...) {
-  p <- VlnPlot(obj, features = feature, pt.size = pt.size, ... )  +
-    xlab("") + ylab(feature) + ggtitle("") +
-    theme(legend.position = "none",
-          axis.text.x = element_blank(),
-          axis.ticks.x = element_blank(),
-          axis.title.y = element_text(size = 20, angle = 0, face = "bold.italic", vjust = 0.5),
-          axis.text.y = element_text(size = 10),
-          plot.margin = plot.margin )
+    p <- VlnPlot(obj, features = feature, pt.size = pt.size, ... )  +
+      xlab("") + ylab(feature) + ggtitle("") +
+      theme(legend.position = "none",
+            axis.text.y = element_blank(),
+            # axis.ticks.x = element_blank(),
+            axis.title.x = element_text(size = 13, angle = 30, face = "italic", vjust = 0.5),
+            axis.title.y = element_text(size = 12, angle = 0, face = "plain", vjust = 0.5),
+            axis.text.x = element_text(size = 8, face = "plain"),
+            plot.margin = plot.margin ) & coord_flip()
+    return(p)
+  }
+
+  ## extract the max value of the y axis
+  extract_max <- function(p){
+    ymax <- max(ggplot_build(p)$layout$panel_scales_y[[1]]$range$range)
+    return(ceiling(ymax))
+  }
+  # 
+  # main code
+  plot_list <- purrr::map(features, function(x) modify_vlnplot(obj = obj,feature = x, ...))
+
+  # Add back x-axis title to bottom plot. patchwork is going to support this?
+  plot_list[[1]] <- plot_list[[1]] +
+    theme(axis.text.y = element_text(angle = 0, size = 15, face = "bold"), axis.ticks.y = element_line())
+
+  # change the y-axis tick to only max value
+  ymaxs <- purrr::map_dbl(plot_list, extract_max)
+  plot_list <- purrr::map2(plot_list, ymaxs, function(x,y) x +
+                             scale_y_continuous(breaks = c(y)) +
+                             expand_limits(y = y))
+
+  p <- patchwork::wrap_plots(plotlist = plot_list, nrow = 1)
+  # p <- cowplot::plot_grid(plotlist = plot_list, ncol = 1, labels = features) # not aligned
   return(p)
 }
 
-## extract the max value of the y axis
-extract_max <- function(p){
-  ymax <- max(ggplot_build(p)$layout$panel_scales_y[[1]]$range$range)
-  return(ceiling(ymax))
-}
-
-
-## main function
-StackedVlnPlot <- function(obj, features,
+StackedVlnPlot.rowGene <- function(obj, features,
                            pt.size = 0,
                            plot.margin = unit(c(-0.75, 0, -0.75, 0), "cm"),
                            ...) {
+  # addition funcitons
+  modify_vlnplot <- function(obj,
+                           feature,
+                           pt.size = 0,
+                           plot.margin = unit(c(-0.75, 0, -0.75, 0), "cm"),
+                           ...) {
+    p <- VlnPlot(obj, features = feature, pt.size = pt.size, ... )  +
+      xlab("") + ylab(feature) + ggtitle("") +
+      theme(legend.position = "none",
+            axis.text.x = element_blank(),
+            axis.ticks.x = element_blank(),
+            axis.title.y = element_text(size = 20, angle = 0, face = "bold.italic", vjust = 0.5),
+            axis.text.y = element_text(size = 10),
+            plot.margin = plot.margin )
+    return(p)
+  }
 
+  ## extract the max value of the y axis
+  extract_max <- function(p){
+    ymax <- max(ggplot_build(p)$layout$panel_scales_y[[1]]$range$range)
+    return(ceiling(ymax))
+  }
+  # 
+  # main code
   plot_list <- purrr::map(features, function(x) modify_vlnplot(obj = obj,feature = x, ...))
 
   # Add back x-axis title to bottom plot. patchwork is going to support this?
